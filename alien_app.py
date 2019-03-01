7350060f6265917a9b39dcab8e1af060e22f4744
stefikolo <DFHBDFG@!$@%235434DFHBR%dfgbdf>
2019-03-01 15:08:27 +0100

    added color

--- alien_app.py
+++ alien_app.py
@@ -9,6 +9,9 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion 3000")
 
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
@@ -16,9 +19,12 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        # Redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
-
+    #TODO 243/276
 
 if __name__ == '__main__':
     run_game()
