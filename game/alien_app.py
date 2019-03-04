ee8c8c441d4daf251911a0538476cfa5f40cb514
stefikolo <DFHBDFG@!$@%235434DFHBR%dfgbdf>
2019-03-04 10:39:06 +0100

    added ship's continous movement

--- game/alien_app.py
+++ game/alien_app.py
@@ -17,6 +17,7 @@ def run_game():
     # Start the main loop for the game.
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(settings, screen, ship)
 
 
