61c5158d5da963a8fb538c22d0b910b50f7e600f
stefikolo <DFHBDFG@!$@%235434DFHBR%dfgbdf>
2019-03-04 13:03:24 +0100

    added bullets to the game - page 261

--- game/alien_app.py
+++ game/alien_app.py
@@ -1,8 +1,8 @@
-import sys
 import pygame
 from game.settings import Settings
 from game.ship import Ship
 import game.game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -14,11 +14,13 @@ def run_game():
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_heigth))
     pygame.display.set_caption("Alien Invasion 3000")
     ship = Ship(screen, settings)
+    bullets = Group()
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ship, settings, screen, bullets)
         ship.update()
-        gf.update_screen(settings, screen, ship)
+        bullets.update()
+        gf.update_screen(settings, screen, ship, bullets)
 
 
 if __name__ == '__main__':
--- game/game_functions.py
+++ game/game_functions.py
@@ -1,25 +1,29 @@
 import sys
 import pygame
+from game.bullet import Bullet
 
 
-def check_events(ship):
+def check_events(ship, settings, screen, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ship, settings, screen, bullets)
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ship, settings, screen, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     if event.key == pygame.K_LEFT:
         ship.moving_left = True
+    if event.key == pygame.K_SPACE:
+        new_bullet = Bullet(settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -29,9 +33,12 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screen(settings, screen, ship):
+def update_screen(settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
     screen.fill(settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
